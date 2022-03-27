a62c39829494066f354169d82738554e8dac8de1
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-03-27 21:30:47 +0100

    chakra ui

---
+++ healthcredit-frontend/src/components/Nav.vue
@@ -0,0 +1,15 @@
+<script>
+import { CFlex } from '@chakra-ui/vue'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <c-flex>
+    <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+  </c-flex>
+</template>
+
+<style scoped>
+
+</style>
--- healthcredit-frontend/src/views/HomeView.vue
+++ healthcredit-frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <header>
+      <p>Hello</p>
+    </header>
   </main>
 </template>
